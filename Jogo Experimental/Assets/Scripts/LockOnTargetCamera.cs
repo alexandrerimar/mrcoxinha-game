a9825ee752174455616074c5d084f9e326373e57
Chevou <alrcintra@gmail.com>
2023-04-04 17:46:39 -0300

    Mudanças

--- Jogo Experimental/Assets/Scripts/LockOnTargetCamera.cs
+++ Jogo Experimental/Assets/Scripts/LockOnTargetCamera.cs
@@ -2,23 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LockOnTarget : MonoBehaviour
+public class LockOnTargetCamera : MonoBehaviour
 {
     public GameObject currentTarget;
     public bool lockOn;
     private Vector3 targetPos;
-    private Vector3 targetPosFromCamera;
     Quaternion rotGoal;
-    Quaternion rotGoalFromCamera;
     [SerializeField] float rotationTime = 0.75f;
-    //[SerializeField] private GameObject mainCamera;
+    [SerializeField] GameObject player;
+    LockOnTarget playerScript;
+   
+
+    void Start () {
+        playerScript = player.GetComponent<LockOnTarget>();
+    }
 
 
     void Update() {
-        if (currentTarget !=null && currentTarget.activeSelf == true){
+        if (currentTarget !=null && currentTarget.activeSelf == true && playerScript.lockOn == true){
             // Desativa o movimento do personagem
             GetComponent<MouseLook>().enabled = false;
-            GetComponent<FPSInput>().enabled = false; 
           
 
             // Pega a posição do inimigo
@@ -32,30 +35,21 @@ public class LockOnTarget : MonoBehaviour
 
 
             // Determina quais eixos vão rotacionas (personagem)
-            Vector3 newRotGoal = new Vector3(transform.rotation.x, rotGoal.y, transform.rotation.z);
+            Vector3 newRotGoal = new Vector3(rotGoal.x, 0, 0);
             Quaternion newQuaternion = new Quaternion(newRotGoal.x, newRotGoal.y, newRotGoal.z, rotGoal.w);            
             
             // Rotaciona personagem
             transform.rotation = Quaternion.RotateTowards(transform.rotation, newQuaternion, speed * Time.deltaTime);
 
-            lockOn = true;
+
                     
         }
         else {
             // Ativa o movimento do personagem
             GetComponent<MouseLook>().enabled = true;
-            GetComponent<FPSInput>().enabled = true;
-            lockOn = false;
         }
     }
     
-    Quaternion GetRotationGoal(Vector3 originPosition) {
-        // Pega a posição do inimigo
-        targetPos = (currentTarget.transform.position - originPosition).normalized;
-        Quaternion rotGoal = Quaternion.LookRotation(targetPos);
-        return(rotGoal);
-    }
-  
 
     public void LockOn()
     {
