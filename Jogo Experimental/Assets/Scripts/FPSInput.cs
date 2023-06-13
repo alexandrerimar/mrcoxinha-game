9cad930915de44da421cc12b8dce0028b7c7319d
Chevou <alrcintra@gmail.com>
2023-06-13 18:59:00 -0300

    Ajusts no Log

--- Jogo Experimental/Assets/Scripts/FPSInput.cs
+++ Jogo Experimental/Assets/Scripts/FPSInput.cs
@@ -27,32 +27,46 @@ public class FPSInput : MonoBehaviour
       gameManagerScript = controller.GetComponent<GameManager>();
     }
 
-    void Update()
+  void Update()
+  {
+    bool wasWalking = isWalking; // Store the previous walking state
+
+    if (gameManagerScript.gameStarted == true)
     {
-      if (gameManagerScript.gameStarted == true)
-      {
-        if (lockOnTargetScript.isLocked == false) {
-          deltaX = Input.GetAxis("Horizontal") * speed;
-          deltaZ = Input.GetAxis("Vertical") * speed;
-        }
-        else {
-          deltaX = 0;
-          deltaZ = 0;
-        }
+      if (lockOnTargetScript.isLocked == false) {
+        deltaX = Input.GetAxis("Horizontal") * speed;
+        deltaZ = Input.GetAxis("Vertical") * speed;
       }
-      Vector3 movement = new Vector3(deltaX, 0, deltaZ);
-      movement = Vector3.ClampMagnitude(movement, speed);
-      movement.y = gravity;
-      movement *= Time.deltaTime;
-      movement = transform.TransformDirection(movement); // Tranforma as coordenadas locais em globais, para passar para como argumento para Move
-      
-      charController.Move(movement); 
-
-      if (deltaX != 0 || deltaZ != 0) {
-        isWalking = true;
-      } else if (deltaX == 0 || deltaZ == 0){
-        isWalking = false;
+      else {
+        deltaX = 0;
+        deltaZ = 0;
       }
-
     }
+
+    Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+    movement = Vector3.ClampMagnitude(movement, speed);
+    movement.y = gravity;
+    movement *= Time.deltaTime;
+    movement = transform.TransformDirection(movement);
+
+    charController.Move(movement);
+
+    if (deltaX != 0 || deltaZ != 0) {
+      isWalking = true;
+    } else {
+      isWalking = false;
+    }
+
+    if (isWalking && !wasWalking) {
+      // Player started walking
+      Logger.Instance.LogAction("Player started walking. Horizontal Axis = " + deltaX + ". Vertical Axis = " + deltaZ);
+    } else if (!isWalking && wasWalking) {
+      // Player finished walking
+      Logger.Instance.LogAction("Player finished walking.");
+    }
+
+    // Log player's position
+    Logger.Instance.LogAction("Player position: " + transform.position);
+
+  }
 }
